13a96f8131ca42935fbf257f972b905b45bb9c43
Dhyanendra-Singh <dhyanendra@kreditz.com>
2025-08-18 13:37:39 +0530

    update the workflow-2

--- flask_app/app.py
+++ flask_app/app.py
@@ -127,7 +127,7 @@ def predict():
     result = model.predict(features_df)
 
     # show
-    return render_template('index.html', result=result[0])
+    return render_template('index.html', result=int(result[0]))
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
